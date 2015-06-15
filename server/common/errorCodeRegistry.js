384c911001d60f927dc84b03ffc9873b04cd9428
Devendra Dhanal <devendradhanal@gmail.com>
2015-06-15 20:41:22 +0530

    Initial commit :relaxed:

---
+++ server/common/errorCodeRegistry.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+  generic: {
+    requestValidationError: 10001,
+  },
+
+  auth: {
+    authTokenRequired: 10101,
+    invalidAuthToken : 10102,
+  },
+
+  user: {
+    create: {
+      validationError      : 11101,
+      usernameAndEmailTaken: 11102,
+      usernameTaken        : 11103,
+      emailTaken           : 11104,
+    },
+    login: {
+      invalidCredentials: 11201,
+      invalidUsername   : 11202,
+      invalidPassword   : 11203,
+    },
+    authToken: {
+      validationError: 11301,
+    },
+  }
+
+};
---
+++ server/data/db.js
@@ -0,0 +1,41 @@
+var _                      = require('lodash')
+  , Promise                = require('bluebird')
+  , mongo                  = require('mongoskin')
+  , db                     = mongo.db('mongodb://localhost:27017/expensesmgr', { native_parser: true })
+  , usersSchema            = require('../api/users/usersSchema');
+
+db.ObjectID = mongo.ObjectID;
+
+db.bind('users').bind({
+  findByUsername: function findByUsername(username, callback) {
+    this.findOne({ username: username }, callback);
+  },
+  findByEmail: function findByEmail(email, callback) {
+    this.findOne({ email: email }, callback);
+  },
+  findByUsernameOrEmail: function findByUsernameOrEmail(usernameOrEmail, callback) {
+    this.findOne({ $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] }, callback);
+  },
+  findByToken: function findByToken(token, callback) {
+    this.findOne({ 'authTokens.token': token }, callback);
+  },
+  updateAuthTokens: function updateAuthTokens(id, authTokens, callback) {
+    var self = this;
+    this.findOne({ _id: id }, function (err, user) {
+      if (err) { return callback(err); }
+      user.authTokens = authTokens;
+      usersSchema.validate(user)
+        .then(function (user) {
+          self.save(user, callback);
+        })
+        .catch(function (err) {
+          return callback(err);
+        });
+    });
+  }
+});
+
+
+Promise.promisifyAll(db.users);
+
+module.exports = db;
